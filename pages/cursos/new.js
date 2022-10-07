5dff5e97200ce2bb2417cca305f1a1c982accb25
Luis Manrique <lpmg11manrique@gmail.com>
2022-10-07 10:06:01 -0600

    adding courses

--- pages/cursos/new.js
+++ pages/cursos/new.js
@@ -5,55 +5,54 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-    const [profesor, setProfesor] = useState({});
+    const [cursos, setCursos] = useState([]);
 
-    const redirectToProfesores = () => {
-        window.location.href = "/profesores";
+    const redirectToCourses = () => {
+        window.location.href = "/cursos";
     };
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setCursos({...cursos, [name]: value});
+        console.log(cursos);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const {name, phone, email} = profesor;
-        axios.post('/api/maestros', {name, phone, email})
+        const {name, schedule, id_teacher} = cursos;
+        axios.post('/api/cursos', {name, schedule, id_teacher})
         .then((res) => {
             console.log(res);
-            redirectToProfesores();
+            redirectToCourses();
         })
         .catch((err) => {
             console.log(err);
         });
     }
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setProfesor({...profesor, [name]: value});
-        console.log(profesor);
-    }
-
-
   return (
     <div>
       <Head>
-        <title>Nuevo Profesor</title>
+        <title>Nuevo curso</title>
         <meta name="description" content="Administracion de colegio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <div className={Style.centerContainer}>
             <div className={Style.formContainer}>
-                <h1>Nuevo Profesor</h1>
+                <h1>Nuevo curso</h1>
                 <form onSubmit={handleSubmit} className={Style.form}>
                     <div className={Style.form_group}>
                         <label htmlFor="name">Nombre:</label>
                         <input onChange={handleChange} type="text" name="name" id="name" placeholder="Nombre" />
                     </div>
                     <div className={Style.form_group}>
-                        <label htmlFor="email">Email:</label>
-                        <input onChange={handleChange} type="email" name="email" id="email" placeholder="Correo" />
+                        <label htmlFor="schedule">Horario:</label>
+                        <input onChange={handleChange} type="number" name="schedule" id="schedule" placeholder="Horario" />
                     </div>
                     <div className={Style.form_group}>
-                        <label htmlFor="phone">Telefono:</label>
-                        <input onChange={handleChange} type="text" name="phone" id="phone" placeholder="Telefono"/>
+                        <label htmlFor="phone">Profesor:</label>
+                        <input onChange={handleChange} type="text" name="id_teacher" id="id_teacher" placeholder="Profesor id"/>
                     </div>
                     <div className={Style.form_group}>
                         <button type="submit">Guardar</button>
